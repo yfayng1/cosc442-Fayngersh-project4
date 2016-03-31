a96cc77a5d0807fea07e4cbef35fa96824b6666d
Fima Fayngersh <yfayng1@students.towson.edu>
2016-03-31 04:28:08 -0400

    Task 4 - Acceptance Testing
    The acceptance tests have been implemented as unit tests. Additional
    tests were written in order to ensure over 90% statement coverage in the
    CoffeeMaker, Inventory, and Recipe classes.

---
+++ CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/AllTests.java
@@ -0,0 +1,11 @@
+package edu.towson.cis.cosc442.project4.coffeemaker;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CoffeeMakerTest.class, InventoryTest.class, RecipeTest.class })
+public class AllTests {
+
+}
